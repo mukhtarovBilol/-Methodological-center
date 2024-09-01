c133be032cc21aa896c5b9f0bf62c1dbb2ee93cd
Bilol <Bilol@gmail.com>
2024-09-01 17:23:02 +0500

    finish site

--- script.js
+++ script.js
@@ -29,3 +29,31 @@ document.addEventListener('DOMContentLoaded', () => {
         burger.classList.toggle('active');
     });
 });
+
+
+var myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    var formData = new FormData(myForm);
+
+    fetch('main.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Ошибка ' + response.status);
+        }
+        return response.text();
+    })
+    .then(function(data) {
+        alert('Данные успешно отправлены');
+        closeModal(); // Закрываем модальное окно после успешной отправки
+    })
+    .catch(function(error) {
+        console.error('Произошла ошибка:', error);
+        alert('Произошла ошибка при отправке данных');
+    });
+});
